09fdbb55d51b516f6503c9e3f927b9022c726cd2
L4M0S <13170563+L4M0S@users.noreply.github.com>
2019-05-23 10:01:35 -0500

    Terminado v3

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
-#include <cstdio>
-#include <cstdlib>
 #include <iostream>
 #include <string>
+#include <cstdio>
+#include <cstdlib>
 #include <set>
 #include <map>
 #include <vector>
@@ -9,16 +9,17 @@
 using namespace std;
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-typedef std::set<int> conjunto;
-typedef std::map<int,conjunto> fila;
-typedef std::map<int, fila> Tabla;
+typedef std::set<int> Casilla;	//conjunto de simbolos generadores de la casilla
+typedef std::map<int,Casilla> Fila;	
+typedef std::map<int, Fila> Tabla;
 
-typedef enum{S=83,R=82,A=65,B=66,C=67,D=68} generadores; //Simbolos generadores  /*simbolo=valor ASCII*/ 
-typedef enum{l=108,x=120,c=99,t=116} terminales; //Simbolos terminales  /*simbolo=valor ASCII*/	
+typedef enum{S=83,R=82,A=65,B=66,C=67,D=68} Generadores; //Simbolos generadores  /*simbolo=valor ASCII*/ 
+typedef enum{l=108,x=120,c=99,t=116} Terminales; //Simbolos terminales  /*simbolo=valor ASCII*/	
 
-Tabla tabla;
+Generadores generdores;
+Terminales terminales;
 
-class Regla
+class Proyeccion
 {
 	public:
 		int generador;
@@ -26,82 +27,99 @@ class Regla
 		int segundo;
 };
 
-typedef std::vector<Regla> Gramatica;
+typedef std::vector<Proyeccion> Gramatica;
+
+Tabla tabla;
+
 Gramatica gramatica;
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-Regla crearRegla(int a, int b, int c=0) /*(generador, primero, segundo) si no hay segundo se omite y solo se colocan generador y primero*/
+Proyeccion proyeccion(int a, int b, int c=0) //(generador, primero, segundo) si no hay segundo se omite y solo se colocan generador y primero
 {
-	Regla regla;
+	Proyeccion proyeccion;
 	
-	regla.generador=a;
-	regla.primero=b;
-	regla.segundo=c;
+	proyeccion.generador=a;
+	proyeccion.primero=b;
+	proyeccion.segundo=c;
 
-	return regla;
+	return proyeccion;
 }
 
-void inicializarGramatica() 
+void definirGramatica()
 {						   	   
-	gramatica.push_back(crearRegla(S,A,C));// S->AC
-	gramatica.push_back(crearRegla(C,S,R));// C->SR
-	gramatica.push_back(crearRegla(R,B,D));// R->BD
-	gramatica.push_back(crearRegla(D,S,R));// D->SR
-	gramatica.push_back(crearRegla(A,l));// A->l
-	gramatica.push_back(crearRegla(S,x));// S->x
-	gramatica.push_back(crearRegla(B,c));// B->c
-	gramatica.push_back(crearRegla(R,t));// R->t
+	gramatica.push_back(proyeccion(S,A,C));	// S->AC
+	gramatica.push_back(proyeccion(C,S,R));	// C->SR
+	gramatica.push_back(proyeccion(R,B,D));	// R->BD
+	gramatica.push_back(proyeccion(D,S,R));	// D->SR
+	gramatica.push_back(proyeccion(A,l));	// A->l
+	gramatica.push_back(proyeccion(S,x));	// S->x
+	gramatica.push_back(proyeccion(B,c));	// B->c
+	gramatica.push_back(proyeccion(R,t));	// R->t
 }
 
-void imprimirGramatica(Gramatica &s) //Imprime la gramatica
+void imprimirGramatica(Gramatica &g) //Imprime la gramatica
 {
 	Gramatica::iterator itr;
-	for (itr=s.begin(); itr!=s.end(); ++itr)
+	for(itr=g.begin(); itr!=g.end(); ++itr)
 	{
-		printf("%c -> %c%c \n", itr->generador, itr->primero, itr->segundo);
+		printf("%c->%c%c\n", itr->generador, itr->primero, itr->segundo);
 	}
 }
 
-bool esMiembro(conjunto &s, int a) //Retorna true si a pertenece al conjunto s
+void reiniciar()	//limpia el contenido de la tabla
 {
-	return s.find(a)!=s.end();
-}
-
-void insertarEnConjunto(conjunto &s, int a) //inserta a en el conjunto s
-{
-	for (int i=0; i<gramatica.size(); i++)
+	for(int i=0;i<tabla.size();i++)
 	{
-		if (gramatica[i].primero==a || gramatica[i].segundo==a) 
+		for(int j=0;j<tabla.size(); j++)
 		{
-			s.insert(gramatica[i].generador);
+			tabla[i][j].clear();
 		}
 	}
 }
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-void insertarEnConjunto(conjunto &s, int a, int b) //inserta a y b en el conjunto s
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+void insertarConjunto(Casilla &c, int a) //inserta el generador de a en el conjunto c // a es terminal //primer columna
 {
-	for (int i=0; i<gramatica.size(); i++)
+	for(int i=0; i<gramatica.size(); i++)
 	{
-		if (gramatica[i].primero==a && gramatica[i].segundo==b) 
+		if(gramatica[i].primero==a)
 		{
-			s.insert(gramatica[i].generador);
+			//printf("gen: %c \n",gramatica[i].generador);
+			c.insert(gramatica[i].generador);
 		}
 	}
+	//cout<<"++++++"<<endl;
 }
 
-void unirConjuntos(conjunto &s, conjunto &s1,conjunto &s2) //une el conjunto s con los conjuntos s1 y s2 
+void insertarConjunto(Casilla &c, int a, int b) //inserta el generador de a y b en el conjunto c // a y b son generadores
 {
-	conjunto::iterator itr1;
-	conjunto::iterator itr2;
+	for(int i=0; i<gramatica.size(); i++)
+	{
+		if(gramatica[i].primero==a && gramatica[i].segundo==b) 
+		{
+			//printf("gen: %c %i \n",gramatica[i].generador, i);
+			c.insert(gramatica[i].generador);
+		}
+	}
+	//cout<<"++++++"<<endl;
+}
+
+void unionConjuntos(Casilla &c, Casilla &c1, Casilla &c2) //une c con los generadores que deriven a la union compuesta por los generadores de c1 y c2 
+{
+	Casilla::iterator itr1;
+	Casilla::iterator itr2;
 	
-	for (itr1=s1.begin(); itr1!=s1.end(); itr1++)
+	for(itr1=c1.begin(); itr1!=c1.end(); itr1++)
 	{
-		for (itr2=s2.begin(); itr2!=s2.end(); itr2++)
+		for(itr2=c2.begin(); itr2!=c2.end(); itr2++)
 		{
-			insertarEnConjunto(s, *itr1, *itr2);
+			//printf("pri: %c seg: %c \n", *itr1, *itr2);
+			insertarConjunto(c, *itr1, *itr2);
 		}
 	}
+	//cout<<"-------"<<endl;
 }
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -110,49 +128,53 @@ bool cyk(Gramatica &g, string &w)
 {
 	int n=w.length();
 
-	for (int i=1; i<=n; i++)
+	for(int i=1; i<=n; i++) //primer columna 
 	{
-		insertarEnConjunto(tabla[i][1],w[i-1]);
+		insertarConjunto(tabla[i][1],w[i-1]);
 	}
 	
-	for (int j=2; j<=n; j++)
+	for(int j=2; j<=n; j++)
 	{
-		for (int i=1; i<=n-j+1; i++)
+		for(int i=1; i<=n-j+1; i++)
 		{
-			for (int k=1; k<=j-1; k++)
+			for(int k=1; k<=j-1; k++)
 			{
-				unirConjuntos(tabla[i][j], tabla[i][k], tabla[i + k][j - k]);
+				unionConjuntos(tabla[i][j], tabla[i][k], tabla[i + k][j - k]);
 			}
 		}
 	}
 
-	if (esMiembro(tabla[1][n], S)) 
-	{
-		return true;
-	}
-	else 
-	{
-		return false;
-	}
+	return tabla[1][n].find(S)!=tabla[1][n].end();
 }
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 int main()
 {
-	inicializarGramatica();
+	definirGramatica();
 	imprimirGramatica(gramatica);
+	
 	string w;	//Cadena a evaluar
-	cin>>w;
+	
+	bool loop=true;
+	
+	do
+	{
+		cout<<"Cadena: ";cin>>w;
+			
+		if(cyk(gramatica, w))
+		{
+			cout<<"aceptada"<<endl;
+		}
+		else
+		{
+			cout<<"rechazada"<<endl;
+		}
 		
-	if (cyk(gramatica, w))
-	{
-		cout<<"aceptada"<<endl;
-	}
-	else
-	{
-		cout<<"rechazada"<<endl;
-	}
-
+		cout<<endl<<"Repetir(1/0): ";cin>>loop;cout<<endl;
+		reiniciar();
+	}while(loop);
+	
+	cout<<endl;
 	system("PAUSE");
 	return 0;
 }
